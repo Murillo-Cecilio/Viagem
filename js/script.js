fec656c7459425ac8488db7b124b347ae122150e
Murillo Cecilio <murilloceciliomoreira@gmail.com>
2023-04-18 23:14:13 -0300

    feito função para adc item a lista

--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
 const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
 
-    console.log(evento);
+    // console.log(evento);
     // console.log(evento.target[0].value);
 
     criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
 });
 
 function criaElemento(nome, quantidade) {
-    console.log(nome);
-    console.log(quantidade);
-
     //<li class="item"><strong>7</strong>Camisas</li>
     const novoItem = document.createElement('li');
     novoItem.classList.add("item");
 
-    const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = quantidade;
 
-    console.log(numeroItem)
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome;
+
+    lista.appendChild(novoItem);
 }
