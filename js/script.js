d47bd36eb4e29426c66048362ecf3d1cfccd3a5b
Murillo Cecilio <murilloceciliomoreira@gmail.com>
2023-04-18 21:37:29 -0300

    criando elementos através do js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,20 @@ form.addEventListener("submit", (evento) => {
 
     console.log(evento);
     // console.log(evento.target[0].value);
-    console.log(evento.target.elements['nome'].value);
-    console.log(evento.target.elements['quantidade'].value);
-})
+
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+});
+
+function criaElemento(nome, quantidade) {
+    console.log(nome);
+    console.log(quantidade);
+
+    //<li class="item"><strong>7</strong>Camisas</li>
+    const novoItem = document.createElement('li');
+    novoItem.classList.add("item");
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    console.log(numeroItem)
+}
