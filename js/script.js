62813d2aa070231df51ea3c1ec895fa292f95df6
Murillo Cecilio <murilloceciliomoreira@gmail.com>
2023-04-18 21:24:27 -0300

    pegando nome dos itens array pelo elements

--- js/script.js
+++ js/script.js
@@ -3,5 +3,8 @@ const form = document.getElementById("novoItem");
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
 
-    console.log("funcionou!");
+    console.log(evento);
+    // console.log(evento.target[0].value);
+    console.log(evento.target.elements['nome'].value);
+    console.log(evento.target.elements['quantidade'].value);
 })
